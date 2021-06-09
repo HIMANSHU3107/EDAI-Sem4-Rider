b09078b067839040a7118f6a1c03d50733793cea
HIMANSHU3107 <himanshubulani07@gmail.com>
2021-06-09 15:17:42 +0530

    First commit

---
+++ cab_rider-master/lib/widgets/TaxiButton.dart
@@ -0,0 +1,28 @@
+import 'package:cab_rider/brand_colors.dart';
+import 'package:flutter/material.dart';
+
+class TaxiButton extends StatelessWidget {
+TaxiButton({this.title, this.color, this. onPressed});
+final String title;
+final Color color;
+final Function onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: Container(
+        height: 50,
+        child: Center(
+          child: Text(
+            title,
+            style: TextStyle(
+              fontSize: 18,
+              fontFamily: 'Brand-Bold',
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
